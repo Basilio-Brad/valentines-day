3e81903a916f00535a45606987dd264b557e3e58
Basilio-Brad <basiliohinostroza2003bradneve@gmail.com>
2024-02-04 17:20:14 -0500

    days

--- js/main.js
+++ js/main.js
@@ -15,8 +15,41 @@ const fn_greats = () => {
 
 document.addEventListener("DOMContentLoaded", () => {
   fn_greats();
+  setInterval(() => {
+    fn_upadteDate();
+  }, 1000);
 });
 
 const fn_redirectToPage = () => {
   window.location.href = "flowers.html";
+};
+
+const valentine_day = new Date("Feb 14, 2024").getTime();
+const id_day = document.getElementById("day");
+const id_hours = document.getElementById("hours");
+const id_minutes = document.getElementById("minutes");
+const id_seconds = document.getElementById("seconds");
+const id_valentinesDay = document.getElementById("valentines-day");
+const id_btnPlay = document.getElementById("btn-play");
+const id_date = document.getElementById("date");
+const fn_upadteDate = () => {
+  const now = new Date().getTime();
+  const distance = (valentine_day - now) / 1000;
+  if (distance > 0) {
+    id_valentinesDay.style.display = "none";
+    id_btnPlay.style.display = "none";
+    date.style.display = "block";
+    const days = Math.floor(distance / 60 / 60 / 24);
+    const hours = Math.floor((distance / 60 / 60) % 24);
+    const minutes = Math.floor((distance / 60) % 60);
+    const seconds = Math.floor(distance % 60);
+    id_day.innerText = days;
+    id_hours.innerText = hours;
+    id_minutes.innerText = minutes;
+    id_seconds.innerText = seconds;
+  } else {
+    id_valentinesDay.style.display = "block";
+    id_btnPlay.style.display = "block";
+    date.style.display = "none";
+  }
 };
